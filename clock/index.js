39548468cd09905d4ddc1413125961f1c0ce3db5
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-09-24 23:28:30 +0300

    clocks: try to localize date & time format && add date

--- clock/index.js
+++ clock/index.js
@@ -22,23 +22,41 @@ const checkTheme = date => {
     document.getElementById('body').classList.add('day')
   }
 }
-
+const setDate = date => {
+  let date_span = document.getElementById('date')
+  date_span.innerText = date.toLocaleDateString('default', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    weekday: 'short'
+  })
+}
 document.addEventListener('DOMContentLoaded', () => {
   let vh = window.innerHeight * 0.01
   document.documentElement.style.setProperty('--vh', `${vh}px`)
   let clock_span = document.getElementById('clock')
-  document.title = clock_span.innerText
+
   let date = new Date()
+  let h = date.getHours()
+  let m = date.getMinutes()
+  let s = date.getSeconds()
+  setDate(date)
+  setTimeout(
+    () => setDate(date),
+    (24 * 60 * 60 - h * 60 * 60 - m * 60 - s) * 1000
+  )
+
   setTimeout(() => {
     setInterval(() => {
       date = new Date()
-      let h = date.getHours()
-      let m = date.getMinutes()
-      let s = date.getSeconds()
+      h = date.getHours()
+      m = date.getMinutes()
+      s = date.getSeconds()
       checkTheme(date)
-      clock_span.innerText = `${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${
-        s < 10 ? '0' + s : s
-      }`
+      // clock_span.innerText = `${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${
+      //   s < 10 ? '0' + s : s
+      // }`
+      clock_span.innerText = date.toLocaleTimeString()
       document.title = clock_span.innerText
     }, 1000)
   }, 1000 - Math.floor(date.getMilliseconds()))
